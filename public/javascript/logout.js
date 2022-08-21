e1aa805369a876fc9a3591c13b79c82dca1b42c0
Matt <mwhlavaty@gmail.com>
2022-08-20 21:13:30 -0700

    fixed routes, added more hbrs

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,4 +1,5 @@
 async function logout() {
+  console.log('made it to logout.js')
   const response = await fetch('/api/users/logout', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' }
@@ -7,7 +8,7 @@ async function logout() {
   if (response.ok) {
     document.location.replace('/');
   } else {
-    alert(response.statusText);
+    document.location.alert('Username and/or email already in use. Please try again.')
   }
 }
 
